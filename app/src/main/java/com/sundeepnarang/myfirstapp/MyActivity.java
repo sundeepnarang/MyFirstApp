0eea5643d9146e91350396c8893005cdd9316169
sundeepnarang <sundeepnrng@gmail.com>
2015-08-26 21:43:38 -0400

    first

---
+++ app/src/main/java/com/sundeepnarang/myfirstapp/MyActivity.java
@@ -0,0 +1,102 @@
+package com.sundeepnarang.myfirstapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+public class MyActivity extends AppCompatActivity {
+    //public final static String EXTRA_MESSAGE = "com.sundeepnarang.myfirstapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    /** Called when the user clicks the Non Violence button */
+    public void openNon(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this,nonV.class);
+        //EditText editText = (EditText) findViewById(R.id.edit_message);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    /** Called when the user clicks the Non Violence button */
+    public void openTru(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this,Tru.class);
+        //EditText editText = (EditText) findViewById(R.id.edit_message);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    /** Called when the user clicks the Non Violence button */
+    public void openCha(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this,Cha.class);
+        //EditText editText = (EditText) findViewById(R.id.edit_message);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    /** Called when the user clicks the Non Violence button */
+    public void openHum(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this,Hum.class);
+        //EditText editText = (EditText) findViewById(R.id.edit_message);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    /** Called when the user clicks the Non Violence button */
+    public void openDiet(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this,Diet.class);
+        //EditText editText = (EditText) findViewById(R.id.edit_message);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    /** Called when the user clicks the Non Violence button */
+    public void openSelf(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this,Self.class);
+        //EditText editText = (EditText) findViewById(R.id.edit_message);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
